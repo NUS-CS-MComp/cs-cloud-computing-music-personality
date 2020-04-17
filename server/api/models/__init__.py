4ddfa24825915c941f2799409cd50e78fb61e2bf
Yap Ying Ying <yapyingying94@gmail.com>
2020-04-18 00:55:15 +0800

    Adding personality insights scores to db

--- server/api/models/__init__.py
+++ server/api/models/__init__.py
@@ -13,6 +13,7 @@ DB = boto3.resource(
 TABLES = [
     {"name": "User", "key_id": "user_id"},
     {"name": "SocialConnect", "key_id": "provider_profile_id"},
+    {"name": "Personality", "key_id": "user_id"},
 ]
 
 
